d7dfab7c9cb3ed363b91dd6664bd14df0f17ab96
996075561 <luana.trindade@estudante.ifms.edu.br>
2023-11-21 08:21:04 -0400

    commit

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,4 +1,30 @@
+
+
+import React, {useState,useEffect} from 'react';
 import './Filme.css'
+import axios from 'axios'
+
+export default function App() {
+   const [data, setData] = useState(null);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+
+useEffect(() => {
+    axios('http://localhost:3000/filmes')
+    .then(response => {
+       setData(response.data)
+    })
+    .catch(error => {
+        console.error("error fetching data: ", error);
+        setError(error);
+    })
+    .finally(() => {
+        setLoading(false);
+    })
+
+}, [])
+if (loading) return "Loading...";
+}
 
 type FilmeProps={
     titulo:string,
